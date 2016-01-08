e139aa8d6591a3e8fefb9883df5fce7ebabf47e1
Shelby Benfield <benfield.shelby@gmail.com>
2016-01-07 23:06:27 -0500

    Update 2.0.0 - Game2 is primary

--- game2.py
+++ game2.py
@@ -1,35 +1,57 @@
 #!/usr/bin/python
 # python game2.py
-
 from sys import *
 
-script, name = argv
-
-# Frequently used strings
 prompt = "> "
 fail = "I don't know that one, try again."
 bye = "Thanks for playing!"
+inventory_list = []
+lives = 3
+swing_exists = True
 
-# Exit options
-def dead(death):
-	print death, "Fantastic!"
-	print "Would you like to try again?"
-	print "'Yes' or 'No'?"
-	restart = raw_input(prompt)
-	if restart.upper() == ("YES"):
+# Game Center
+def game():
+	choice = raw_input(prompt).upper()
+	# Level 1
+	if choice == "BEGIN" or choice == "RETURN" or choice == "START":
 		start()
-	elif restart.upper() == ("NO"):
-		exit(bye)
-	else:
+	# Level 2
+	elif choice == "NORTH" or choice == "GRAVEYARD":
+		north()
+	elif choice == "SOUTH" or choice == "YARD" or choice == "LADDER":
+		south()
+	# Level 3
+	elif choice == "EYES":
+		eyes()
+	elif choice == "GRAVES":
+		graves()
+	elif choice == "SIT" and swing_exists == True:
+		sit()
+	elif choice == "HOUSE":
+		house()
+	# Level 4
+	elif choice == "LIGHTS":
+		lights()
+	elif choice == "SWING":
+		swing()
+	elif choice == "CARVINGS":
+		carving()
+	elif choice == "TUNNEL":
+		tunnel()
+	# Character Options
+	elif choice == "INVENTORY" or choice == "LIVES" or choice == "CHARACTER":
+		character()
+	# Exit Options
+	elif choice  == "QUIT":
 		leave()
+	else:
+		begin()
 
-def leave():
-	print "bye"
-	exit(bye)
-
-
-# Game Begin and Housekeeping
+# Main Game Functions
 def begin():
+	global lives
+	lives = 3
+
 	print "Welcome to Demon's Run"
 	print "We are very glad to have you."
 	print "A few housekeeping tips to make your stay enjoyable."
@@ -37,132 +59,128 @@ def begin():
 	print "   Those words are commands you can give the game to make decisions."
 	print "2. At any time, you can exit the game by simply typing 'quit'."
 	print "3. Anything you run into can be killed or subdued, if you only have the resources."
-	print "4. At any time, as long as you're not dead, you can go backward, simply type 'back' or 'run'"
+	print "4. At any time, as long as you're not dead, you can restart, simply type 'return' or 'start'."
 	print "5. Have fun and don't do anything I wouldn't do."
-	print "Ready to start (Y/N)?"
-	begin = raw_input(prompt)
-	if begin.upper() == ("Y" or "YES"):
-		start()
-	elif begin.upper()  == ("QUIT" or "N" or "NO"):
-		leave()
+	print "6. You can check the status of your character at any time, simply type 'lives', 'character', or 'inventory'."
+	print "Ready to 'begin' or would you like to 'quit'?"
+	game()
 
-# Game Start
+# Level 1
 def start():
 	print "Hello, %s, how are you?" % name
 	print "You have awoken from a trance."
 	print "You are standing in a clearing at night,"
 	print "a path goes 'north' to 'south', which path do you take?"
+	game()
 
-	start = raw_input(prompt)
+#Level 2	
+def north():
+	print "You are standing in a graveyard." 
+	print "The graves look as though they haven't been touched in decades."
+	print "You see red glowing eyes in the treeline."
+	print "You can examine the 'graves', go toward the 'eyes', or 'return' to the clearing, what do you do?"
+	game()
 
-	path(start)
+def south():
+	print "You are on the front porch of an older, antebellum house."
+	print "It has a wooden, but comfortable-looking swing you can 'sit' in."
+	print "The door is slightly open, and you can enter the 'house'." 
+	print "What do you do?"
+	game()
 
-	exit()
+# Level 3
+def eyes():
+	print "You reach the space where you thought the eyes were,"
+	print "but nothing is there. Far in the distance, you see what looks like the same eyes."
+	print "You can go forward, to the 'lights' or you can go back to the 'graveyard', what do you do?"
+	game()
 
+def graves():
+	print "You examine the graves and find a fist-sized stone."
+	inventory("STONE")
+	north()
 
-def path(direction):
-	if direction.upper() == "NORTH" or direction.upper() == "N":
-		print "The path leads to a graveyard." 
-		print "The graves look as though they haven't been touched in decades."
-		print "You see red glowing 'eyes' in the treeline."
-		print "You can go forward toward the 'eyes' or 'back' to the clearing, what do you do?"
-	
-		eyes_choice = raw_input(prompt)
-
-		eyes(eyes_choice)
-
-	elif direction.upper() == "SOUTH" or direction.upper() == "S":
-		print "You come upon an older, antebellum house."
-		print "It has a metal, but very rusted, but comfortable looking 'swing'."
-		print "The 'door' is slightly open, and you can enter the house. What do you do?"
-
-		house_choice = raw_input(prompt)
-		
-		house(house_choice)
-	elif direction.upper() == "QUIT":
-		leave()
-	else :
-		start()
-
-
-def house(choice):
-	if choice.upper() == "SWING" or choice.upper() == "SIT":
+def sit():
+	print "Swing:", swing_exists
+	if swing_exists == True:
 		print "You sit on the swing."
 		print "It barely supports you, but is a good place to rest."
 		print "You can 'swing' on the swing, but it seems unsafe."
-		print "You see 'carvings' in the arms of the swing."
-		print "What do you do?"
+	print "You see 'carvings' in the wooden arms of the swing."
+	print "What do you do?"
+	game ()
 
-		swing_choice = raw_input(prompt)
-
-		swing(swing_choice)
-
-	elif choice.upper() == "DOOR" or choice.upper() == "HOUSE" or choice.upper() == "ENTER":
-		print "You push the door open, and are inside a large foyer."
-		print "There is a rug immediately inside the door on the floor."
-		print "Stepping on the rug, you feel a slight give in the floor, just before you fall through it."
-		print "You find yourself in a 'tunnel' that looks fairly modern and continues into darkness."
-		print "The tunnel has a 'ladder' back to the surface."
-		print "What do you do?"
-
-		tunnel_choice = raw_input(prompt)
-
-		tunnel(tunnel_choice)
-	elif choice.upper() == ("BACK" or "RUN"):
-		start()
-	elif choice.upper() == ("QUIT"):
-		leave()
-	else:
-		path("S")
+def house():
+	print "You push the door open, and are inside a large foyer."
+	print "There is a rug immediately inside the door on the floor."
+	print "Stepping on the rug, you feel a slight give in the floor, just before you fall through it."
+	print "You find yourself in a 'tunnel' that looks fairly modern and continues into darkness."
+	print "The tunnel has a 'ladder' back to the surface."
+	print "What do you do?"
+	game ()
 
 
-def tunnel(choice):
-	if choice.upper() == "DARK" or choice.upper() == "TUNNEL" or choice.upper() == "TOWARD":
-		# make a fight with something?
-		dead("You are eaten by a grue.")
-	elif choice.upper == "LADDER" or choice.upper() == "RUN" or choice.upper() == "BACK":
-		path("S")
-	elif choice.upper() == "QUIT":
-		leave()
-	else:
-		exit(dead(fail))
+# Level 4
+def carving():
+	print "You feel a strange sensation in your mind, as though it is being molded by tentacles."
+	print "You have gained an ability called 'sleep'. It's pretty self-explanatory."
+	inventory("SLEEP")
+	print "You stand and move away from the swing."
+	south()
 
-def swing(choice):
-	if choice.upper() == "SWING":
-		# make a resource for a stake.
-		dead("That was really dumb. You're now impaled on the wreckage of the swing.")
-	elif choice.upper() == "READ" or  choice.upper() == "CARVINGS":
-		# make a resource for a spell.
-		dead("Your brain is turned to mush and pours out of your eyes. Cthulhu has been summoned.")
-	elif choice.upper() == "BACK" or choice.upper() == "RUN":
-		path("S")
-	elif choice.upper() == "QUIT":
-		leave()
-	else:
-		exit(dead(fail))
+def lights():
+	dead("You trip on a root and die, the lights you saw were lightning bugs. You're colorblind.")
 
 
-def eyes(choice):	
-	if choice.upper() == "EYES" or choice.upper() == "TREELINE":
-		print "You reach the space where you thought the eyes were,"
-		print "but nothing is there. Far in the distance, you see what looks like the same eyes."
-		print "You can go toward the 'eyes' or you can go back to the 'graveyard', what do you do?"
-		
-		eyes2 = raw_input(prompt)
-		
-		if eyes2.upper() == "EYES" or eyes2.upper() == "FORWARD":
-			dead("You trip on a root and die, the lights you saw were lightning bugs. You're colorblind.")
-		elif eyes2.upper() == "GRAVEYARD" or eyes2.upper() == "BACK" or eyes2.upper() == "PATH":
-			path("EYES")
-		elif choice.upper() == "QUIT":
-			leave()
-		else:
-			exit(dead(fail))
-	elif choice.upper() == "BACK" or choice.upper() == "PATH" or choice.upper() == "CLEARING":
-		start()
-	elif choice.upper() == "QUIT":
-		leave()
-	else:
-		exit(dead(fail))
+def swing():
+	print "That was really dumb. You're now sitting on the wreckage of the old swing."
+	print "A very sharp piece, almost a stake of wood sits beside you."
+	print "You pick up the wooden stake."
+	swing_exists = False
+	print "Swing exists:", swing_exists
+	inventory("STAKE")
+	print "You are sitting on the pile of wood."
+	sit()
 
+def tunnel():
+	dead("You are eaten by a grue.")
+
+
+
+# Character Management
+def inventory(choice):
+	print "Adding %s to inventory." % choice
+	inventory_list.append(choice)
+	print "Your inventory contains: ", inventory_list
+
+# Exit Functions
+def dead(death):
+	print death, "Fantastic!"
+	life()
+
+def leave():
+	print "bye"
+	exit(bye)
+
+# Character Management
+def character():
+	print "Your name is %s." % name
+	print "Your inventory contains: ", inventory_list
+	print "You have %s lives." % lives
+	game()
+
+def life():
+	global lives
+
+	if lives > 0:
+		lives = lives - 1
+		print "You have %s lives left." % lives
+		print "Would you like to 'start' again?"
+		game()
+
+	if lives == 0:
+		print "Game over!", bye
+
+print "What is your name?"
+name = raw_input()
 begin()
